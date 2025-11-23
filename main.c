6a23a81af8488685d1c3fb3aecbba625157f7929
Francesco Cannistrà <francescor.cannistra@davincimilazzo.edu.it>
2025-11-23 19:25:21 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "funzioni.h"
+
+int addizione();
+int sottrazione();
+int moltiplicazione();
+int divisione();
+
+int main() {
+    char scelta;
+    int ris;
+    int risultato=0;
+
+    printf("Scegli l'operazione:\n");
+    printf("addizione = Q\nsottrazione = W\nmoltiplicazione = E\ndivisione = S\n");
+    printf("Inserisci la tua scelta: ");
+    scanf(" %c", &scelta); 
+
+    if (scelta == 'Q' || scelta == 'W' || scelta == 'E' || scelta == 'S') {
+        if (scelta == 'Q')
+            risultato = addizione();
+        else if (scelta == 'W')
+            risultato = sottrazione();
+        else if (scelta == 'E')
+            risultato = moltiplicazione();
+        else if (scelta == 'S')
+            risultato = divisione();
+
+        printf("Risultato: %d\n", risultato);
+    } 
+
+    return 0;
+}
